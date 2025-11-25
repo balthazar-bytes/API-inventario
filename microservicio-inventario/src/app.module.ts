576efe8fcadac87b3aa6b04ce49b128c36dbe2d4
Balthazar-Martinez <balthazar@uali.co>
2025-11-24 22:25:05 -0300

    creacion del controller con un dto

--- microservicio-inventario/src/app.module.ts
+++ microservicio-inventario/src/app.module.ts
@@ -1,9 +1,10 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { InventarioModule } from './inventario/inventario.module';
 
 @Module({
-  imports: [],
+  imports: [InventarioModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- microservicio-inventario/src/main.ts
+++ microservicio-inventario/src/main.ts
@@ -1,8 +1,14 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { ValidationPipe } from '@nestjs/common';
+
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.useGlobalPipes(new ValidationPipe({
+    whitelist: true, // Borra propiedades que no estén en el DTO (limpieza)
+    forbidNonWhitelisted: true, // Lanza error si envían datos basura extra
+  }));
   await app.listen(process.env.PORT ?? 3000);
 }
 bootstrap();
